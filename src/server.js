e7a03d3a64e389dd0d49e822d14af9de1547bde5
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-26 19:31:17 -0300

    método get

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+
+app.get("/", (request, response) => {
+    response.send("Hello World");
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
