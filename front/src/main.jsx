7bac6feac21056a7c6600cf3927c34c21c974935
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-30 18:56:45 -0300

    styles global

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details/index'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+
+import theme from './styles/theme'
+
+
+import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Details />
+        <ThemeProvider theme={theme}>
+            <GlobalStyles/>
+            <Details /> 
+        </ThemeProvider>
     </React.StrictMode>
 )
--- front/src/pages/Details/styles.js
+++ front/src/pages/Details/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-    background: red;
+   
 
 `
