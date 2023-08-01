e6d62c9c454d236385383d69ff2dc5ab7323a47f
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-01 15:50:04 -0300

    header

--- front/src/components/Header/styles.js
+++ front/src/components/Header/styles.js
@@ -17,3 +17,34 @@ export const Container = styled.head`
 
     background: red;
 `
+
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+
+    // Alterar a imagem de dentro da div
+    // div > img
+    > img {
+        width: 56px;
+        height: 56px;
+        border-radius: 50%; // imagem ficar redonda
+    }
+
+    > div {
+        display: flex;
+        flex-direction: column; // uma ao lado da outra
+        margin-left: 16px;
+        line-height: 24px;
+
+        // div > div > span
+        span {
+            font-size: 14px;
+            color: ${({ theme }) => theme.COLORS.GRAY_100}
+        }
+        // div > div > strong
+        strong {
+            font-size: 14px;
+            color: ${({ theme }) => theme.COLORS.WHITE}
+        }
+    }
+`
