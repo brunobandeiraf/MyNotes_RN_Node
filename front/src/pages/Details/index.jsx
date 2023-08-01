f85a258896eac311fc3d1339a6f9a975b9616a1e
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-07-31 22:32:46 -0300

    button

--- front/src/pages/Details/index.jsx
+++ front/src/pages/Details/index.jsx
@@ -1,10 +1,15 @@
 import { Container } from './styles'
 
+import { Button } from '../../components/Button'
+
 export function Details(){
     
     return(
         <Container>
             <h1>Olá</h1>
+            <span>Bruno Bandeira</span>
+
+            <Button/>
         </Container>
     )
 }
