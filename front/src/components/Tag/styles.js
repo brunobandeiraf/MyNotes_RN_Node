faa0cce9ded8a5b8ac12cc59a157b6262ec08562
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-02 00:49:30 -0300

    tag

---
+++ front/src/components/Tag/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.span`
+    font-size: 12px;
+    padding: 5px 14px;
+    border-radius: 6px;
+    margin-right: 6px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+`
--- front/src/pages/Details/index.jsx
+++ front/src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details(){
     
@@ -15,6 +16,11 @@ export function Details(){
                     <li> <a href="#"> Link 1</a> </li>
                     <li> <a href="#"> Link 2</a> </li>
                 </Links>
+            </Section>
+
+            <Section title="Marcadores">
+                <Tag title="express"/>
+                <Tag title="nodejs"/>
             </Section>
 
             <h1>Olá</h1>
