e5a92297fb599255310362d6c31381b6b62f29e9
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-02 22:38:54 -0300

    buttontext

---
+++ front/src/components/ButtonText/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+
+export function ButtonText({ title, isActive = false, ...rest }){
+    return(
+        <Container
+            type="button"
+            $isactive={isActive.toString()}
+            {...rest}
+        >
+            { title }
+        </Container>
+    )
+}
--- front/src/pages/Details/index.jsx
+++ front/src/pages/Details/index.jsx
@@ -4,12 +4,15 @@ import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details(){
     
     return(
         <Container>
-             <Header/>
+            <Header/>
+            
+            <ButtonText title="Excluir nota"/>
             
             <Section title="Links úteis">
                 <Links>
