fa89fb01b4bc03b60107583d5e173cf10f8f2a0b
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-02 23:25:38 -0300

    home

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -6,13 +6,14 @@ import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
 
-import { Details } from './pages/Details'
+// import { Details } from './pages/Details'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <GlobalStyles/>
-            <Details /> 
+            <Home /> 
         </ThemeProvider>
     </React.StrictMode>
 )
