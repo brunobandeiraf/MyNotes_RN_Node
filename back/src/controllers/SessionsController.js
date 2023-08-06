4795b21dfcca734d53d7226d2537697940bdc0af
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-06 19:23:17 -0300

    session controllers

--- back/src/controllers/SessionsController.js
+++ back/src/controllers/SessionsController.js
@@ -1,8 +1,27 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
+
 class SessionsController{
     async create(request, response){
         const { email, password } = request.body
 
-        return response.json({ email, password })
+        const user = await knex("users").where({ email }).first()
+
+        // Se usuário não existe
+        if(!user){
+            throw new AppError("E-mail e/ou senha incorreta", 401)
+        }
+
+        // Usa o bcrypt para comparar as senhas
+        const passwordMatched = await compare(password, user.password)
+        // Se senha não confere com a senha cadastrada
+        if(!passwordMatched){
+            throw new AppError("E-mail e/ou senha incorreta", 401)
+        }
+
+
+        return response.json(user)
     }
 }
 
