dccab0638c3dd20f95f0c8ca9f18484b7dfcf342
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-09 18:27:49 -0300

    upload

--- back/src/configs/upload.js
+++ back/src/configs/upload.js
@@ -1,16 +1,20 @@
-const TMP_FOLDER  =  require("path")
+const path  =  require("path")
+
 const multer = require("multer")
 const crypto = require("crypto")
 
-const TMP_FOLDER = path.resolver(__dirname, "..", "..", "temp")
+const TMP_FOLDER = path.resolver(__dirname, "..", "..", "tmp")
 const UPLOADS_FOLDER = path.resolver(__dirname, "uploads")
 
 const MULTER = {
     storage: multer.diskStorage({
+        // Destino do armazenamento
         destination: TMP_FOLDER, 
+        // Nome do arquivo
+        // Hash crypto para garantir um nome único
         filename(request, file, callback){
             const fileHash = crypto.randomBytes(10).toString("hex")
-            const filaName = `${fileHash} - ${file.originalname}`
+            const fileName = `${fileHash} - ${file.originalname}`
 
             return callback(null, fileName)
         }
