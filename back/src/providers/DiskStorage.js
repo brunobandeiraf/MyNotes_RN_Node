c720d7cf9c9ee0f7f318cb9b7afdebc9cff24574
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-09 18:39:05 -0300

    manipulando arquivos

--- back/src/providers/DiskStorage.js
+++ back/src/providers/DiskStorage.js
@@ -5,6 +5,7 @@ const uploadConfig = require("../configs/upload")
 // Quando o arquivo é carregado, ele é enviado para uma pasta temporária
 class DiskStorage{
     async saveFile(file){
+        // rename - renomear ou mover o arquivo
         await fs.promises.rename(
             // Busca a origem do arquivo salvo
             path.resolve(uploadConfig.TMP_FOLDER, file),
@@ -20,12 +21,13 @@ class DiskStorage{
         const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
 
         try{
+            // Retorna o status do arquivo
             await fs.promises.stat(filePath)
         }catch{
             return
         } 
 
-        // Deletar o arquivo
+        // Remover o arquivo
         await fs.promises.unlink(filePath)
     }
 }
