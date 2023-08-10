035e1d7afbc8f785a34eb24877058d1017832102
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-10 14:04:59 -0300

    redirecionamento rotas login

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 
+import { useAuth } from '../hooks/auth'
+ 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes(){
+    // Acessando usuário do useAuth - usuário autenticado
+    const { user } = useAuth()
+
     return(
         <BrowserRouter>
-            {/* <AppRoutes/> */}
-            <AuthRoutes/>
+            { user ? <AppRoutes/> : <AuthRoutes/>}
         </BrowserRouter>
+               
+        // Se tiver user - acessa AppRoutes - rotas autenticadas
+        // Se não tiver, acessa AuthRoutes - rotas não autenticadas
+            
     );
 };
