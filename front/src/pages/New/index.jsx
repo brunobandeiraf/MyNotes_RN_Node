5c150a16d6a66a76771c09c007338c7d47acfe6d
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-12 13:22:00 -0300

    validando campos da nota

--- front/src/pages/New/index.jsx
+++ front/src/pages/New/index.jsx
@@ -50,6 +50,19 @@ export function New(){
     }
 
     async function handleNewNote(){
+
+        if(!title){
+            return alert("Digite o título da nota.")
+        }
+        
+        if(newLink){
+            return alert("Você deixou um link no campo para adicionar, mas não clicou em adicionar. /n Clique para adicionar ou deixe o campo vazio")
+        }
+
+        if(newTag){
+            return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. /n Clique para adicionar ou deixe o campo vazio")
+        }
+
         await api.post("/notes", {
             title, 
             description, 
