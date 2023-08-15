5b988de1a468c0ccaf41330569ce115d59a31636
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-15 11:29:19 -0300

    baseurl

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-    // Endereço da API
-    baseURL: "http://localhost:3333"
+    // Endereço da API - localhost
+    //baseURL: "http://localhost:3333"
+    // Disponível pelo Render
+    baseURL: "https://mynotes-xjhu.onrender.com"
 })
