f2cb5efef4bbf4da34f13541e28b00a38866f8c7
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-15 12:42:17 -0300

    inversão de dependencia

--- back/src/controllers/UsersController.js
+++ back/src/controllers/UsersController.js
@@ -1,14 +1,17 @@
 const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
+
 const sqliteConnection = require("../database/sqlite")
+const UserRepository = require("../repositories/UserRepository")
 
 class UsersController {
 
     async create (request, response){
         const { name, email, password } = request.body
 
-        const database = await sqliteConnection()
-        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+        const userRepository = new UserRepository()
+        
+        const checkUserExists = await userRepository.findByEmail(email)
 
         if(checkUserExists){
             throw new AppError("Este e-mail já está em uso.")
@@ -17,10 +20,7 @@ class UsersController {
         // Criptografando a senha com bcrypt
         const hashedPassword = await hash(password, 8)
 
-        await database.run(
-            "INSERT INTO users (name, email, password) VALUES (?,?,?)", 
-            [name, email, hashedPassword]
-        )
+        await userRepository.create({ name, email, password: hashedPassword })
 
         return response.status(201).json()
     }
