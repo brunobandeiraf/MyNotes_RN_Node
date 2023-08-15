286726c9ab0f6b8f6ccb3f9bb2ff9d5943105c3b
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-15 13:08:24 -0300

    test de cadastro

--- back/src/services/UserCreateService.js
+++ back/src/services/UserCreateService.js
@@ -1,4 +1,4 @@
-const { hash, compare } = require("bcryptjs")
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 // Cuida dq regra de negócio da aplicação
@@ -19,7 +19,9 @@ class UserCreateService {
         // Criptografando a senha com bcrypt
         const hashedPassword = await hash(password, 8)
 
-        await this.userRepository.create({ name, email, password: hashedPassword })
+        const userCreated = await this.userRepository.create({ name, email, password: hashedPassword })
+        
+        return userCreated
     }
 }
 
--- back/src/services/userCreateService.spec.js
+++ back/src/services/userCreateService.spec.js
@@ -1,7 +1,27 @@
-it("Result of them sum of 2 + 2 must be 4", () =>{
-    const a = 2
-    const b = 2
-    const result = a + b
+const UserCreateService = require("./UserCreateService")
 
-    expect(result).toEqual(4) // Espero que o resultado seja 4
+it("User should be create", () => {
+    // Simula os dados de entrada do usuário
+    const user = {
+        name: "User Test",
+        email: "user@test.com",
+        password: "1234"
+    }
+
+    // Para testes é importante ter um repositório em memória para isolar o banco dos testes
+    const userCreateService = new UserCreateService()
+    // Passo o user test para o service do Usuário
+    const userCreated = userCreateService.execute(user)
+
+    // 
+    expect(userCreated).toHaveProperty("id")
+
 })
+
+// it("Result of them sum of 2 + 2 must be 4", () =>{
+//     const a = 2
+//     const b = 2
+//     const result = a + b
+// toEqual ou toBe
+//     expect(result).toEqual(4) // Espero que o resultado seja 4
+// })
